bb9d7bfd78b87fee7472ccee2f6213647c827826
George Albuquerque Pinto <georgealbuquerquepinto@hotmail.com>
2020-09-24 16:08:05 -0300

    Estrutura de repetição FOR encadeado

---
+++ AprendendoCSharp/P12-CalculaInvestimentoLongoPrazo/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P12_CalculaInvestimentoLongoPrazo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando projeto 12 - Calcula investimento longo prazo");
+
+            double valorInvertido = 1000;
+            double fatorRendimento = 1.0036;
+
+            for (int contadorAno = 1; contadorAno <= 5; contadorAno++)
+            {
+                for (int contadorMes = 1; contadorMes <= 12; contadorMes++)
+                {
+                    valorInvertido *= fatorRendimento;
+                }
+
+                fatorRendimento += 0.0010;
+            }
+
+            Console.WriteLine("Após o investimento você terá: R$ " + valorInvertido);
+
+            Console.WriteLine("A execução terminou. Precione Enter para sair.");
+            Console.ReadLine();
+        }
+    }
+}
