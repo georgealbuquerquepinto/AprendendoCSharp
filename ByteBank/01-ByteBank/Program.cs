6dc3410b18ee7e5f8645bb495aa325269d3fa5db
George Albuquerque Pinto <georgealbuquerquepinto@hotmail.com>
2020-09-25 15:27:28 -0300

    Cria uma classe
    A classe ContaCorrente é criada

---
+++ ByteBank/01-ByteBank/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente contaGabriela = new ContaCorrente();
+
+            contaGabriela.titular = "Gabriela";
+            contaGabriela.agencia = 863;
+            contaGabriela.numero = 863452;
+            contaGabriela.saldo = 100;
+
+            Console.WriteLine(contaGabriela.titular);
+            Console.WriteLine("Agência: " + contaGabriela.agencia);
+            Console.WriteLine("Número: " + contaGabriela.numero);
+            Console.WriteLine("Saldo: R$ " + contaGabriela.saldo);
+
+            contaGabriela.saldo += 200;
+
+            Console.WriteLine("Saldo: R$ " + contaGabriela.saldo);
+
+            Console.ReadLine();
+        }
+    }
+}
